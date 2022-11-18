7aefacc98daf8375a9653920c5b976a0cf579c8e
Ethan BEN ATTAR <ethan95.benattar@gmail.com>
2022-11-18 12:36:43 +0200

    Add grequests for faster scraping

--- scrapper_rt.py
+++ scrapper_rt.py
@@ -1,3 +1,4 @@
+import grequests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,22 +12,35 @@ import argparse
 TIME_TO_SLEEP = 3
 
 
+def send_thread_request(urls):
+    my_requests = (grequests.get(url) for url in urls)
+    responses = grequests.map(my_requests)
+    for response in responses:
+        movie_info = get_movie_info(response.text)
+        print(movie_info)
+
+
 def get_movies_url(text):
     soup = BeautifulSoup(text)
     main_movies = soup.find_all('a', {'class': 'js-tile-link', 'href': True})
     return main_movies
 
 
-def get_movie_info(content, movie_info):
-    try:
-        movie_info[content.find('div').string] = content.find('div', {
-            'class': 'meta-value'}).string.strip()
-    except AttributeError:
-        if content.find('div', {'class': 'meta-value'}).find_all('a'):
-            movie_info[content.find('div').string] = [c.string.strip() for c in
-                                                      content.find('div',
-                                                                   {'class': 'meta-value'})
-                                                      .find_all('a')]
+def get_movie_info(text):
+    movie_soup = BeautifulSoup(text)
+    movie_info = {'Title': movie_soup.find('score-board').find('h1').string,
+                  'Tomatometer': movie_soup.find('score-board').get('tomatometerscore')}
+    movie_infos = movie_soup.find('ul', {'class': 'content-meta info'}).find_all('li')
+    for content in movie_infos:
+        try:
+            movie_info[content.find('div').string] = content.find('div', {
+                'class': 'meta-value'}).string.strip()
+        except AttributeError:
+            if content.find('div', {'class': 'meta-value'}).find_all('a'):
+                movie_info[content.find('div').string] = [c.string.strip() for c in
+                                                          content.find('div',
+                                                                       {'class': 'meta-value'})
+                                                          .find_all('a')]
     return movie_info
 
 
@@ -34,19 +48,15 @@ def get_all_movies_info(text):
     """
     This function scrap the "AT HOME" movies from ROTTEN TOMATOES
     """
+    packed_urls = []
     try:
         movies = get_movies_url(text)
         # Let's go to the movie page to catch contents
         for movie_url in movies:
-            movie_page = requests.get('https://www.rottentomatoes.com' + movie_url['href'])
-            if movie_page.ok:
-                movie_soup = BeautifulSoup(movie_page.text)
-                movie_info = {'Title': movie_soup.find('score-board').find('h1').string,
-                              'Tomatometer': movie_soup.find('score-board').get('tomatometerscore')}
-                movie_infos = movie_soup.find('ul', {'class': 'content-meta info'}).find_all('li')
-                for content in movie_infos:
-                    movie_info = get_movie_info(content, movie_info)
-                print(movie_info)
+            packed_urls.append('https://www.rottentomatoes.com' + movie_url['href'])
+            if len(packed_urls) == 5:
+                send_thread_request(packed_urls)
+                packed_urls = []
     except ValueError as err:
         print(err)
 
@@ -68,6 +78,7 @@ def get_page(page_number):
     text = wd.page_source
     get_all_movies_info(text)
     wd.close()
+    exit()
 
 
 def main():
@@ -75,7 +86,7 @@ def main():
     parser.add_argument('Number_of_pages', type=int)
     args = parser.parse_args()
     page_number = args.Number_of_pages
-    get_page(page_number)
+    get_page(page_number - 1)
 
 
 if __name__ == '__main__':
